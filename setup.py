9f600deb34f2831e6720e901715f7d069bdb1be6
“rdeshmukh15” <rutuja.deshmukh@qlik.com>
2025-07-31 12:02:26 +0530

    library updates

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-ujet',
-      version='1.0.0',
+      version='1.1.0',
       description='Singer.io tap for extracting data from the UJET API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_ujet'],
       install_requires=[
-          'backoff==1.8.0',
-          'requests==2.24.0',
-          'singer-python==5.9.0'
+          'backoff==2.2.1',
+          'requests==2.32.4',
+          'singer-python==6.1.1'
       ],
       extras_require={
           'dev': [
