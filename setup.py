a41823d6b275f64a0d27253beb04a097882f54ab
Ben Allred <ben.allred@qlik.com>
2025-12-09 10:27:28 -0700

    bump version for compliance failures (#12)
    * bump version for compliance failures
    
    * unpin pip

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-ujet',
-      version='1.0.0',
+      version='1.0.1',
       description='Singer.io tap for extracting data from the UJET API',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_ujet'],
       install_requires=[
-          'backoff==1.8.0',
-          'requests==2.24.0',
-          'singer-python==5.9.0'
+          'backoff==1.10.0',
+          'requests==2.32.5',
+          'singer-python==5.14.3'
       ],
       extras_require={
           'dev': [
